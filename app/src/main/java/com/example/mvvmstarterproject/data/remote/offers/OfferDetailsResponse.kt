9b051c5e9c4d827162494910c2a84a8b99082c68
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-12-06 14:41:44 +0200

    [add] add order details response

---
+++ app/src/main/java/com/example/mvvmstarterproject/data/remote/offers/OfferDetailsResponse.kt
@@ -0,0 +1,34 @@
+package com.example.mvvmstarterproject.data.remote.offers
+
+
+import com.google.gson.annotations.SerializedName
+
+data class OfferDetailsResponse(
+    @SerializedName("brand")
+    val brand: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("expiration")
+    val expiration: String?,
+    @SerializedName("favoriteCount")
+    val favoriteCount: Int?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("imageUrl")
+    val imageUrl: String?,
+    @SerializedName("price")
+    val price: Price?,
+    @SerializedName("redemptionsCap")
+    val redemptionsCap: String?,
+    @SerializedName("tags")
+    val tags: String?,
+    @SerializedName("title")
+    val title: String?
+) {
+    data class Price(
+        @SerializedName("new")
+        val new: String?,
+        @SerializedName("old")
+        val old: String?
+    )
+}

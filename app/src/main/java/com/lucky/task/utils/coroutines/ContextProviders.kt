96660969ba16aa32f61e32d8eb3152fa3dbbd0ec
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-12-08 18:02:45 +0200

    [add] comment context provider file

--- app/src/main/java/com/lucky/task/utils/coroutines/ContextProviders.kt
+++ app/src/main/java/com/lucky/task/utils/coroutines/ContextProviders.kt
@@ -3,6 +3,10 @@ package com.lucky.task.utils.coroutines
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
+/*
+* wrap coroutin context for testing purposes
+* https://proandroiddev.com/how-to-unit-test-code-with-coroutines-50c1640f6bef
+* */
 open class ContextProviders {
     open val Main: CoroutineContext = Dispatchers.Main
     open val IO: CoroutineContext = Dispatchers.IO
